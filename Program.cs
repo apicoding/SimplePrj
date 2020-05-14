da506a7d8ae8db1300ab5c12a578bf92ca086743
apicoding <apicodingprj@gmail.com>
2020-05-14 20:56:42 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace SimplePrj
+{
+    interface IPrint
+    {
+        void Print();
+    }
+
+    class Printer : IPrint
+    {
+        // method missing here
+    }
+
+    class Program
+    {
+        public static void Main()
+        {
+            new Printer().Print();
+        }
+    }
+}
